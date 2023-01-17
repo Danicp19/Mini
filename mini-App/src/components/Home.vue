123216751ecfab38eb8eb04f5320f83b8485eb66
Daniela Camargo <daniela.camargopr@gmail.com>
2023-01-17 11:55:09 -0500

    first commit

---
+++ mini-App/src/components/Home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center">
+    <router-link to="/miniApp/">
+      <button
+        class="mt-6 bg-blue-300 p-1.5 px-5 absolute rounded-md text-white shadow hover:shadow-md bg-opacity-90"
+      >
+        Start a new meeting
+      </button>
+    </router-link>
+  </div>
+</template>
+<style scoped>
+.ghost {
+  @apply bg-slate-400 opacity-30;
+}
+</style>
